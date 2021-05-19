622f100ca5463de8ada5a35e5944634e875ec153
Denis <zzomrot.ne@gmail.com>
2021-05-19 14:54:48 +0300

    Added tests template

--- test/FeeCollector.js
+++ test/FeeCollector.js
@@ -6,6 +6,7 @@ const ethSigUtil = require('eth-sig-util');
 const Wallet = require('ethereumjs-wallet').default;
 
 const TokenMock = artifacts.require('TokenMock');
+const FeeCollector = artifacts.require('FeeCollector');
 
 const { EIP712Domain, domainSeparator } = require('./helpers/eip712');
 const { profileEVM, gasspectEVM } = require('./helpers/profileEVM');
@@ -23,13 +24,19 @@ contract('FeeCollector', async function ([_, wallet]) {
     const account = Wallet.fromPrivateKey(Buffer.from(privatekey, 'hex'));
 
     const zeroAddress = '0x0000000000000000000000000000000000000000';
-    const name = '1inch Limit Order Protocol';
+    const name = '1inch FeeCollector';
     const version = '1';
+
+    const minValue = '1000000';
+    const maxValue = '1000000000000000000000';
+    const deceleration = '1000000000';
 
     beforeEach(async function () {
         this.dai = await TokenMock.new('DAI', 'DAI');
         this.weth = await TokenMock.new('WETH', 'WETH');
 
+        this.feeCollector = await FeeCollector.new(this.dai.address, minValue, maxValue, deceleration);
+        
         // We get the chain id from the contract because Ganache (used for coverage) does not return the same chain id
         // from within the EVM as from the JSON RPC interface.
         // See https://github.com/trufflesuite/ganache-core/issues/515
@@ -39,6 +46,14 @@ contract('FeeCollector', async function ([_, wallet]) {
         await this.weth.mint(wallet, '1000000');
         await this.dai.mint(_, '1000000');
         await this.weth.mint(_, '1000000');
+    });
+
+    describe('PriceForTime', async function () {
+        it('Started price', async function () {
+            const startedTime = await this.feeCollector.started.call()
+            const price = await this.feeCollector.priceForTime.call(startedTime);
+            expect(price.toString()).equal(maxValue);
+        });
     });
 
     describe('Something', async function () {
