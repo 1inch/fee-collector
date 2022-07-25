16f02ae22d51a22551181c622cdf6b2db7a2e6ff
Denis <zzomrot.ne@gmail.com>
2022-07-26 02:29:15 +0300

    Reused TokenMock and reuse helper methods from solidity-utils

--- contracts/mocks/TokenMock.sol
+++ contracts/mocks/TokenMock.sol
@@ -2,26 +2,13 @@
 
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@1inch/solidity-utils/contracts/mocks/TokenMock.sol";
 import "../interfaces/IFeeCollector.sol";
 
 
-contract TokenMock is ERC20, Ownable {
+contract TokenWithUpdateRewardMock is TokenMock {
     // solhint-disable-next-line no-empty-blocks
-    constructor(string memory name, string memory symbol) ERC20(name, symbol) {}
-
-    function mint(address account, uint256 amount) external onlyOwner {
-        _mint(account, amount);
-    }
-
-    function burn(address account, uint256 amount) external onlyOwner {
-        _burn(account, amount);
-    }
-
-    function getChainId() external view returns (uint256) {
-        return block.chainid;
-    }
+    constructor(string memory name, string memory symbol) TokenMock(name, symbol) {}
 
     function updateReward(IFeeCollector _feeCollector, address referral, uint256 amount) public {
         transfer(address(_feeCollector), amount);
